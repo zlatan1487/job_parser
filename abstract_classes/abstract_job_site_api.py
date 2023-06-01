7205353e4f48fe821ad79fe7d1851880663e8185
zlatan1487 <vitalik.karpukhin@list.ru>
2023-06-02 00:16:37 +0200

    created the main logic of the project

--- abstract_classes/abstract_job_site_api.py
+++ abstract_classes/abstract_job_site_api.py
@@ -2,9 +2,6 @@ from abc import ABC, abstractmethod
 
 
 class AbstractJobSiteAPI(ABC):
-    """
-        абстрактный класс для работы с апи платформ
-    """
     @abstractmethod
     def get_vacancies(self, keyword):
         pass
--- abstract_classes/abstract_saver.py
+++ abstract_classes/abstract_saver.py
@@ -1,10 +1,21 @@
 from abc import ABC, abstractmethod
+from colorama import init, Fore
 
 
 class AbstractSaver(ABC):
-    """
-       абстрактный класс для работы с файлом json
-    """
+    def __init__(self, filename):
+        self.filename = filename
+
+    @staticmethod
+    def print_vacancies(vacancies):
+        print(f'{Fore.YELLOW}FOUND {len(vacancies)} VACANCIES')
+        for vacancy in vacancies:
+            print(f'{Fore.LIGHTWHITE_EX}title:', f'{Fore.GREEN}', vacancy['title'])
+            print(f'{Fore.LIGHTWHITE_EX}link:', f'{Fore.GREEN}', vacancy['link'])
+            print(f'{Fore.LIGHTWHITE_EX}salary:', f'{Fore.GREEN}', vacancy['salary'])
+            print(f'{Fore.LIGHTWHITE_EX}description:', f'{Fore.GREEN}', vacancy['description'])
+            print(f"{Fore.RED}{175 * '*'}")
+
     @abstractmethod
     def add_vacancy(self, vacancy_data):
         pass
@@ -12,3 +23,6 @@ class AbstractSaver(ABC):
     @abstractmethod
     def get_vacancies_by_salary(self, salary_range):
         pass
+
+
+
